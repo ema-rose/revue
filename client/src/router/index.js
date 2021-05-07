425190eeed92669b1072243262d379da385188bc
Emily Schroeder <erschroeder@knox.edu>
2021-05-06 21:41:36 -0600

    temp fix home error

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,6 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router';
 
-import Home from '@/views/Home'
+//import Home from '@/views/Home'
 
 // GUIDE: This file defines urls for the frontend
 const routes = [
