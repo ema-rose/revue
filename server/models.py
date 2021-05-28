4207031d62ae7ba6af485069b88fc15fe39c8200
Joe Pederson <joepederson@Joes-MacBook-Air.local>
2021-05-28 16:17:10 -0500

    added mongo points

--- server/models.py
+++ server/models.py
@@ -3,14 +3,14 @@ import hashlib
 import os
 
 from mongoengine import (
-    connect, Document, EmailField, StringField, ListField, ReferenceField, DateTimeField, EmbeddedDocument,
+    connect, Document, EmailField, StringField, FloatField, ListField, ReferenceField, DateTimeField, EmbeddedDocument,
     EmbeddedDocumentField, CASCADE
 )
 
 # GUIDE: Here we are connecting to the mongodb database. os.environ.get is getting environment variables
 # if they are not set, the values are None which leads us to login to the default database.
-username = os.environ.get('MONGODB_USERNAME')
-password = os.environ.get('MONGODB_PASSWORD')
+username = os.environ.get('MONGODB_USERNAME','flaskuser')
+password = os.environ.get('MONGODB_PASSWORD','passwd123')
 host = os.environ.get('MONGODB_HOSTNAME')
 db = os.environ.get('MONGODB_DATABASE', "revue")
 connect(username=username, password=password, host=host, db=db)
@@ -38,6 +38,57 @@ class User(Document):
 
         return data
 
+class Point(Document):
+    title = StringField(required=True, unique=True)
+    description = StringField(required=True)
+    latitude = FloatField(required=True)
+    longitude = FloatField(required=True)
+    created = DateTimeField(required=True, default=datetime.datetime.now())
+
+    def to_public_json(self):
+        data = {
+            "title": self.title,
+            "description": self.description,
+            "latitude": self.latitude,
+            "longitude": self.longitude,
+        }
+        return data
+
+#point1 = Point(title='Alumi Hall', description='Admissions and admistrative offices', latitude=-90.371893, longitude=40.943456)
+#point1.save()
+
+#point2 = Point(title='Seymour Libary', description='Main libary on campus', latitude=-90.372998, longitude=40.943112)
+#point2.save()
+
+#point3 = Point(title='Seymour Hall', description='Cafeteria and dorms as well as career center', latitude=-90.372199, longitude=40.942661)
+# point3.save()
+
+# point4 = Point(title='Old Main', description='Site of the Lincoln Douglas debate', latitude=-90.37115, longitude=40.943122)
+# point4.save()
+
+# point5 = Point(title='Ford Center of Fine Arts', description='harbach theatre as well as recital hall', latitude=-90.370211, longitude=40.941767)
+# point5.save()
+
+# point6 = Point(title='George Davis Hall', description='acidemic building for social science classes', latitude=-90.370163, longitude=40.943443)
+# point6.save()
+
+# point7 = Point(title='Umbeck Science-Mathematics Center', description='acidemic building for science and math classes', latitude=-90.375109, longitude=40.943134)
+# point7.save()
+
+# point8 = Point(title='Memorial Gym', description='Gym with weight room, basketball court, and pool', latitude=-90.369385, longitude=40.940915)
+# point8.save()
+
+# point9 = Point(title='T. Fleming Fieldhouse', description='indoor track and field house', latitude=-90.368683, longitude=40.94075)
+# point9.save()
+
+# point10 = Point(title='Knosher Bowl', description='football field', latitude=-90.371966, longitude=40.939628)
+# point10.save()
+
+# point11 = Point(title='Prats Field', description='soccer field', latitude=-90.373409, longitude=40.939612)
+# point11.save()
+
+# point12 = Point(title='Me', description='Where I live currently', latitude=-90.373553, longitude=40.941729)
+# point12.save()
 
 class Comment(EmbeddedDocument):
     content = StringField(max_length=5000)
